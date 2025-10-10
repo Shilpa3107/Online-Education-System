1506cd7ccfe141c285906e8def943071fe48ad90
Shilpa3107 <shilpa.sinha3107@gmail.com>
2025-10-10 08:18:27 +0530

    Implement React Navigation

--- App.js
+++ App.js
@@ -1,70 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View , Image} from 'react-native';
 import Home from './src/screens/Home';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 
 export default function App() {
-  const description = "We are committed to providing quality education to learners worldwide. Our platform offers a wide range of courses, expert instructors, and flexible learning options to help you achieve your educational goals.";
-  return (
-    <View style = {styles.mainContainer}>
-    <View style = {styles.homeTop}>
-      <Image style={styles.headerImage}
-      resizeMode='contain'
-      source={require('./assets/logo.jpg')}
-      />
-      <Text style={styles.mainHeader}>Welcome to</Text>
-      <Text style={[
-        styles.mainHeader,{
-           fontSize: 33,
-           color: '#4c5dab',
-           marginTop: 0
-      }
-      ]}>Online Education System</Text>
-      <Text style = {styles.paraStyle}>{ description }</Text>
-    </View>
 
-    </View>
-  );
+  const Stack = createNativeStackNavigator();
+
+  return <NavigationContainer>
+    <Stack.Navigator initialRouteName='Home'>
+      <Stack.Screen name="Home">
+      {(props) =><Home {...props} channelName={'Online Edu System'} />}
+      </Stack.Screen>
+    </Stack.Navigator>
+  </NavigationContainer>
 }
-
-const styles = StyleSheet.create({
- mainContainer:{
-  height: '100%',
-  display: 'flex',
-  justifyContent: 'space-between',
-  paddingHorizontal: 20,
-  backgroundColor: '#fff',
-  textAlign: 'center',
- },
-
- homeTop:{
-  display: 'flex',
-  justifyContent:'center',
-  alignItems: 'center',
-  paddingHorizontal:10,
- },
-
- headerImage:{
-  height: undefined,
-  width: '100%',
-  aspectRatio : 1,
-  display: 'flex',
-  alignItems:'stretch',
-  marginTop: 50,
-  borderRadius:40
- },
-
- mainHeader:{
-  fontSize: 30,
-  color: '#344055',
-  textTransform: 'uppercase',
- },
-
- paraStyle:{
-  textAlign: 'left',
-  fontSize: 19,
-  color: '#7d7d7d',
-  marginTop:30,
-  paddingBottom:50,
-  lineHeight: 26,
- }
-});
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,14 +1,74 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 import React from "react";
 
-const Home = () => {
-    return(
-        <View>
-            <Text>Welcome to Home Page</Text>
-        </View>
-    )
+
+const Home = (props) => {
+const description = "We are committed to providing quality education to learners worldwide. Our platform offers a wide range of courses, expert instructors, and flexible learning options to help you achieve your educational goals.";
+  return (
+    <View style = {styles.mainContainer}>
+    <View style = {styles.homeTop}>
+      <Image style={styles.headerImage}
+      resizeMode='contain'
+      source={require('../../assets/logo.jpg')}
+      />
+      <Text style={styles.mainHeader}>Welcome to</Text>
+      <Text style={[
+        styles.mainHeader,{
+           fontSize: 33,
+           color: '#4c5dab',
+           marginTop: 0
+      }
+      ]}>{props.channelName}</Text>
+      <Text style = {styles.paraStyle}>{ description }</Text>
+    </View>
+
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+ mainContainer:{
+  height: '100%',
+  display: 'flex',
+  justifyContent: 'space-between',
+  paddingHorizontal: 20,
+  backgroundColor: '#fff',
+  textAlign: 'center',
+ },
+
+ homeTop:{
+  display: 'flex',
+  justifyContent:'center',
+  alignItems: 'center',
+  paddingHorizontal:10,
+ },
+
+ headerImage:{
+  height: undefined,
+  width: '100%',
+  aspectRatio : 1,
+  display: 'flex',
+  alignItems:'stretch',
+  marginTop: 50,
+  borderRadius:40
+ },
+
+ mainHeader:{
+  fontSize: 30,
+  color: '#344055',
+  textTransform: 'uppercase',
+ },
+
+ paraStyle:{
+  textAlign: 'left',
+  fontSize: 19,
+  color: '#7d7d7d',
+  marginTop:30,
+  paddingBottom:50,
+  lineHeight: 26,
+ }
+});
+
 
 export default Home;
 
-const styles = StyleSheet.create({})
